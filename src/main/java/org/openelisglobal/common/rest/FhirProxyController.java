d6f815ccb46cf929d647d9faa052a3a99f24f0d1
pc-dissanayake <sadpcd@duck.com>
2025-11-20 05:15:06 +0100

    Update FHIR proxy to use local FHIR server endpoint

--- src/main/java/org/openelisglobal/common/rest/FhirProxyController.java
+++ src/main/java/org/openelisglobal/common/rest/FhirProxyController.java
@@ -1,22 +1,9 @@
 package org.openelisglobal.common.rest;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.TrustStrategy;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,10 +16,10 @@ public class FhirProxyController {
 
     @GetMapping("/fhir/patients")
     public ResponseEntity<String> getPatients(@RequestParam(defaultValue = "_count=50&_sort=-_lastUpdated") String query) {
-        String fhirUrl = "https://38.242.212.117/fhir/Patient?" + query;
+        String fhirUrl = "http://fhir.openelis.org:8080/fhir/Patient?" + query;
 
         try {
-            RestTemplate restTemplate = createRestTemplateWithTrustAll();
+            RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
             headers.set("Accept", "application/fhir+json");
             HttpEntity<String> entity = new HttpEntity<>(headers);
@@ -42,24 +29,5 @@ public class FhirProxyController {
         } catch (Exception e) {
             return ResponseEntity.status(500).body("{\"error\": \"" + e.getMessage() + "\"}");
         }
-    }
-
-    private RestTemplate createRestTemplateWithTrustAll() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLSocketFactory(csf)
-                .build();
-
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-
-        return new RestTemplate(requestFactory);
     }
 }
